b829c6b88180a6432d427547048da6912efc4be4
Sam Sanchez <WearyCodes@gmail.com>
2023-08-23 11:35:24 -0500

    finally got it

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 import './index.less'
-import './components/article/article.js'
+import articlestuff from './components/article/article.js'
 import './components/menu/menu.js'
+
+// function articleMaker(articlestuff){
+//     const articlenode = document.createElement('div')
+//      articlenode.classList.add('article')
+//     const titlenode = document.createElement('h2')
+//     titlenode.textContent = articlestuff.title
+//    const datenode = document.createElement('p')
+//    datenode.classList.add('date')
+//    datenode.textContent = articlestuff.date
+//        const firstparagraph = articlestuff.firstparagraph
+//        const secondparagraph = articlestuff.secondParagraph
+//        const thirdparagraph = articlestuff.thirdParagraph
+//     const expandButton = document.createElement('span')
+//     expandButton.classList.add('expandButton')
+//     expandButton.textContent = '+'
+
+//     articlenode.appendChild(titlenode);
+//     articlenode.appendChild(datenode);
+//     articlenode.appendChild(firstparagraph);
+//     articlenode.appendChild(expandButton);
+// expandButton.addEventListener('click', () => {
+//     document.querySelector('.div article').classList.toggle('article-open')
+// })
+// return articlenode
+// }
+
+
+// articlestuff.forEach(article => {
+//     articleMaker(article)
+// })
+
+
